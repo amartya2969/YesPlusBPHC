9e63eb1d4d678958f77a13b7c99e3271eedcd310
amartya2969 <amartya2969@gmail.com>
2018-06-22 00:13:39 +0530

    Update 3

--- app.py
+++ app.py
@@ -8,5 +8,12 @@ def main():
 @app.route("/Experiences")
 def Experiences():
     return render_template('Experiences.html')
+@app.route("/Workshops")
+def Workshops():
+    return render_template('Upcoming_Workshops.html')
+@app.route("/Contacts")
+def Contact_Us():
+    return render_template('Contact_Us.html')
+
 if __name__=="__main__":
     app.run()
